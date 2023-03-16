3ea92c87c40b6145d58f97da40cf326f96482f4a
MohanadJay <mohanadjayousi@mermir.com>
2023-03-16 13:15:09 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head> 
+
+<title> this is me  </title>
+<link rel="stylesheet" href="/app.css" > 
+</head> 
+
+
+<body> 
+
+<h1> this is me agian <h1> 
+<body> 
+</html>
